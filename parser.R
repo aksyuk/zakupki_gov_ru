72d77e9dfd004fa252d6f3f6e80b69a091685e8f
aksyuk <s.a.aksuk@gmail.com>
2017-09-04 19:13:01 +0300

    parser v 0.9
    Parser of contracts completed.

--- parser.R
+++ parser.R
@@ -33,6 +33,7 @@ const.html <- c('http://zakupki.gov.ru/epz/contract/quicksearch/search.html?sear
                 'sortDirection',
                 'recordsPerPage',
                 'sortBy',
+                'fz44',
                 'priceFrom',
                 'priceTo',
                 'budgetLevels',
@@ -52,6 +53,9 @@ const.html <- c('http://zakupki.gov.ru/epz/contract/quicksearch/search.html?sear
                 'publishDateTo',
                 'updateDateFrom',
                 'updateDateTo')
+
+## имя браузера для маскировки запроса по getURL()
+userAgent <- 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'
 
 ## имя файла для экспорта
 exportCsvFileName <- paste0('zakupki_gov_ru_', Sys.time(), '.csv')
@@ -86,6 +90,7 @@ var.html <- c('',                                       # searchString
               'false',                                  # sortDirection
               '_50',                                    # recordsPerPage
               'PO_DATE_OBNOVLENIJA',                    # sortBy
+              'on',                                     # fz44
               '0',                                      # priceFrom
               '1000000',                                # priceTo
               '',                                       # budgetLevels
@@ -101,8 +106,8 @@ var.html <- c('',                                       # searchString
               '',                                       # contractDateTo
               '',                                       # contractInputNameDefenseOrderNumber
               '',                                       # contractInputNameContractNumber
-              '01.01.2016',                             # publishDateFrom
-              '29.08.2017',                             # publishDateTo
+              '01.06.2017',                             # publishDateFrom
+              '31.08.2017',                             # publishDateTo
               '',                                       # updateDateFrom
               ''                                        # updateDateTo
               ) 
@@ -112,17 +117,6 @@ fileURL <- paste0(paste0(const.html, '=', var.html), collapse = '&')
 
 # ЗАГРУЗКА ---------------------------------------------------------------------
 #
-## создаём эмулятор браузера, т.к. сайт госзакупок хитровывернутый
-RSelenium::rsDriver()
-remDr <- remoteDriver(port = 4444, browserName = "chrome")
-remDr$open(silent = T)
-remDr$navigate(fileURL)
-
-#!!!!!!
-# НУЖНО ЭТО ДЕЛАТЬ ЧЕРЕЗ Docker
-# https://stackoverflow.com/questions/45395849/cant-execute-rsdriver-connection-refused
-#!!!!!!
-
 ## загружаем текст html-страницы
 html <- getURL(fileURL)
 ## разбираем как html
@@ -133,130 +127,252 @@ rootNode <- xmlRoot(doc)
 
 # ПАРСИНГ ----------------------------------------------------------------------
 #
+# РЕЕСТР КОНТРАКТОВ ============================================================
+#
 ## настроечные переменные
 #...............................................................................
-### номер страницы выдачи
-k <- 1
-# порядковый номер первой записи в таблице на странице
-num.start <- k
+
+### сколько страниц в выдаче
+n.pages <- as.numeric(xpathApplyAndClean(rootNode, 
+                                         "//div[@class = 'paginator greyBox']/ul/li"))
+n.pages <- n.pages[!is.na(n.pages)]
+n.pages <- max(n.pages)
+
 # количество записей на одной странице
 n.at.one.page <- as.numeric(substr(var.html[5], 2, 
                                    nchar(var.html[5])))
-# порядковый номер последней записи в таблице на странице
-num.end <- k + n.at.one.page - 1
 
-## парсим страницу с выдачей во фрейм df.part
-#...............................................................................
-### 1 столбец -- порядковый номер записи в общей таблице
-df.part <- data.frame(Num = seq(num.start, num.end, 1))
 
-### 2 столбец -- статус заказа
-df.part$Status <- 
-    xpathApplyAndClean(rootNode, "//dd[@class='status-field']")
+### цикл по номерам страниц выдачи
+for (k in 1:1) {
+    # порядковый номер первой записи в таблице на странице
+    num.start <- k
+    
+    # порядковый номер последней записи в таблице на странице
+    num.end <- k + n.at.one.page - 1
+    
+    ## парсим страницу с выдачей во фрейм df.part
+    #...............................................................................
+    ### 1 столбец -- порядковый номер записи в общей таблице
+    df.part <- data.frame(Num = seq(num.start, num.end, 1))
+    
+    ### 2 столбец -- статус заказа
+    df.part$Status <- 
+        xpathApplyAndClean(rootNode, "//dd[@class='status-field']")
+    
+    ### 3 столбец -- итоговая сумма заказа (предложение победителя)
+    df.part$FinalStoim <- 
+        xpathApplyAndClean(rootNode, "//td[@class = 'tenderTd']/dl/dd/strong")
+    
+    ### 4 столбец -- номер контракта
+    df.part$ContractID <- 
+        xpathApplyAndClean(rootNode, "//td[@class='descriptTenderTd']/dl/dt")
+    
+    ### 5 столбец -- ссылка на контракт
+    df.part$ContractHyperlink <- 
+        xpathApplyAndClean(rootNode, "//td[@class='descriptTenderTd']/dl/dt/a",
+                           'attr', 'href')
+    
+    ### 6 столбец -- ссылка на заказчика
+    df.part$customerHyperlink <- 
+        xpathApplyAndClean(rootNode, "//td[@class='descriptTenderTd']/dl/dd[@class='nameOrganization']/a",
+                           'attr', 'href')
+    
+    ### 7 столбец -- наименование заказчика
+    df.part$NameCustomer <- 
+        xpathApplyAndClean(rootNode, 
+                           "//td[@class='descriptTenderTd']/dl/dd[@class='nameOrganization']")
+    
+    ### 8 столбец (заготовка) -- ОКТМО заказчика
+    df.part$CustomerOKTMO <- rep('', n.at.one.page)
         
-### 3 столбец -- итоговая сумма заказа (предложение победителя)
-df.part$FinalStoim <- 
-    xpathApplyAndClean(rootNode, "//td[@class = 'tenderTd']/dl/dd/strong")
-
-### 4 столбец -- номер контракта
-df.part$ContractID <- 
-    xpathApplyAndClean(rootNode, "//td[@class='descriptTenderTd']/dl/dt")
-
-### 5 столбец -- ссылка на контракт
-df.part$ContractHyperlink <- 
-    xpathApplyAndClean(rootNode, "//td[@class='descriptTenderTd']/dl/dt/a",
-                       'attr', 'href')
-
-### 6 столбец -- наименование заказчика
-df.part$NameCustomer <- 
-    xpathApplyAndClean(rootNode, 
-                       "//td[@class='descriptTenderTd']/dl/dd[@class='nameOrganization']")
-
-### с датами обновления и размещения приходится потанцевать
-tmp <- xpathApplyAndClean(rootNode, 
-                          "//label[@class='width80']/parent::li")
-tmp.1 <- gsub(gsub(tmp[grepl(tmp, pattern = 'Размещено:')],
-                   pattern = 'Размещено:', replacement = ''),
-              pattern = '^\\s+|\\s+$', replacement = '')
-tmp.2 <- gsub(gsub(tmp[grepl(tmp, pattern = 'Обновлено:')],
-                   pattern = 'Обновлено:', replacement = ''),
-              pattern = '^\\s+|\\s+$', replacement = '')
-
-### 7 столбец -- дата размещения заказа
-df.part$PostedDate <- tmp.1
-
-### 8 столбец -- дата обновления заказа
-df.part$UpdatedDate <- tmp.2
-
-## находим переходим по ссылке к описанию электронного аукциона
-details.URLs <- xpathApplyAndClean(rootNode, 
-                                   "//dd[@class = 'additionalDescriptionList']//a",
-                                   'attr', 'href')
-
-### 9 столбец (заготовка) -- наименование электронной площадки
-df.part$PlatformName <- rep('', n.at.one.page)
-
-### 10 столбец (заготовка) -- объект закупки: наименование
-df.part$ObjectName <- rep('', n.at.one.page)
-
-### 11 столбец (заготовка) -- объект закупки: единица измерения
-df.part$ObjectUnits <- rep('', n.at.one.page)
-
-### 12 столбец (заготовка) -- объект закупки: количество единиц
-df.part$ObjectAmount <- rep('', n.at.one.page)
-
-### 13 столбец (заготовка) -- объект закупки: цена за единицу
-df.part$ObjectUnitPrice <- rep('', n.at.one.page)
-
-### 14 столбец (заготовка) -- начальная (максимальная) цена контракта
-df.part$ObjectNMZ <- rep('', n.at.one.page)
-
-### 15 столбец (заготовка) -- ценовое предложение участника
-df.part$ParticipantPriceOffer <- rep('', n.at.one.page)
-
-### 16 столбец (заготовка) -- наименование участника
-df.part$ParticipantName <- rep('', n.at.one.page)
-
-### 17 столбец (заготовка) -- флаг: победитель
-df.part$ParticipantIsWinner <- rep(F, n.at.one.page)
-
-## парсим детали
-for (i in 1:length(details.URLs)) {
-    ## загружаем текст html-страницы
+    
+    ### с датами обновления и размещения приходится потанцевать
+    tmp <- xpathApplyAndClean(rootNode, 
+                              "//label[@class='width80']/parent::li")
+    tmp.1 <- gsub(gsub(tmp[grepl(tmp, pattern = 'Размещено:')],
+                       pattern = 'Размещено:', replacement = ''),
+                  pattern = '^\\s+|\\s+$', replacement = '')
+    tmp.2 <- gsub(gsub(tmp[grepl(tmp, pattern = 'Обновлено:')],
+                       pattern = 'Обновлено:', replacement = ''),
+                  pattern = '^\\s+|\\s+$', replacement = '')
+    
+    ### 9 столбец -- дата размещения заказа
+    df.part$PostedDate <- tmp.1
+    
+    ### 10 столбец -- дата обновления заказа
+    df.part$UpdatedDate <- tmp.2
+    
+    ## находим переходим по ссылке к описанию электронного аукциона
+    details.URLs <- xpathApplyAndClean(rootNode, 
+                                       "//dd[@class = 'additionalDescriptionList']/ul/li/span/a",
+                                       'attr', 'href')
+    
+    ### 11 столбец (заготовка) -- наименование электронной площадки
+    df.part$PlatformName <- rep('', n.at.one.page)
+    
+    ### 12 столбец (заготовка) -- объект закупки: наименование
+    df.part$ObjectName <- rep('', n.at.one.page)
+    
+    ### 13 столбец (заготовка) -- объект закупки: код по ОКПД2
+    df.part$ObjectCode <- rep('', n.at.one.page)
+    
+    ### 14 столбец (заготовка) -- объект закупки: единица измерения
+    df.part$ObjectUnits <- rep('', n.at.one.page)
+    
+    ### 15 столбец (заготовка) -- объект закупки: количество единиц
+    df.part$ObjectAmount <- rep('', n.at.one.page)
+    
+    ### 16 столбец (заготовка) -- объект закупки: цена за единицу
+    df.part$ObjectUnitPrice <- rep('', n.at.one.page)
+    
+    ### 17 столбец (заготовка) -- начальная (максимальная) цена контракта
+    df.part$ObjectNMZ <- rep('', n.at.one.page)
+    
+    ### 18 столбец (заготовка) -- есть ли ограничения допуска
+    df.part$IsRestrictions <- rep(F, n.at.one.page)
+    
+    ### 19 столбец (заготовка) -- минимальное ценовое предложение
+    df.part$MinPriceOffer <- rep('', n.at.one.page)
+    
+    ### 20 столбец (заготовка) -- максимальное ценовое предложение
+    df.part$MinPriceOffer <- rep('', n.at.one.page)
+    
+    ### 21 столбец (заготовка) -- количество участников
+    df.part$ParticipantsNumber <- rep('', n.at.one.page)
+    
+    
+    # ссылки на страницы с общей информацией
     destFileURL <- paste0('http://zakupki.gov.ru', details.URLs)
     destFileURL <- gsub(destFileURL, pattern = '/notice/view/',
                         replacement = '/notice/ea44/view/')
     
-    getURL('http://zakupki.gov.ru/epz/order/notice/ea44/view/common-info.html?regNumber=0163200000316006223',
-           .encoding = 'UTF-8')
+    # сведения о поставщиках: гиперссылки
+    destFileURL2 <- paste0('http://zakupki.gov.ru/epz/order/notice/ea44/view/supplier-results.html?regNumber=', 
+                           gsub('.*=', '', destFileURL))
     
-    destHtml <- getURL(destFileURL[i], .encoding = 'UTF-8')
+    ## парсим детали
+    for (i in 1:length(details.URLs)) {
+        
+        print(paste0('i = ', i))
+        print(destFileURL[i])
+        
+        destHtml <- getURL(destFileURL[i], .encoding = 'UTF-8',
+                           ssl.verifypeer = FALSE, 
+                           .opts = list(useragent = userAgent,
+                                        followlocation = TRUE))
+        
+        ## разбираем как html
+        descDoc <- htmlTreeParse(destHtml, useInternalNodes = T)
+        ## корневой элемент
+        descRootNode <- xmlRoot(descDoc)
+        
+        # всё содержимое табличек, потому что я не могу нормально 
+        #  запарсить по условию
+        raw.notice <- xpathApplyAndClean(descRootNode, 
+                                         "//div[@class = 'noticeTabBoxWrapper']/table//td")
+        
+        ### 11 столбец -- наименование электронной площадки
+        df.part$PlatformName[i] <- raw.notice[which(grepl(raw.notice, 
+                                                          pattern = 'Наименование электронной площадки')) + 1]
+        
+        ### 12 столбец -- объект закупки: наименование
+        df.part$ObjectName[i] <- raw.notice[which(grepl(raw.notice, 
+                                                        pattern = 'Наименование товара, работы, услуги')) + 8][1]
+        
+        ### 13 столбец -- объект закупки: код по ОКПД2
+        df.part$ObjectCode <- raw.notice[which(grepl(raw.notice, 
+                                                     pattern = 'Наименование товара, работы, услуги')) + 9][1]
+        
+        ### 14 столбец -- объект закупки: единица измерения
+        df.part$ObjectUnits[i] <- raw.notice[which(grepl(raw.notice, 
+                                                         pattern = 'Наименование товара, работы, услуги')) + 10][1]
+        
+        ### 15 столбец -- объект закупки: количество единиц
+        df.part$ObjectAmount[i] <- raw.notice[which(grepl(raw.notice, 
+                                                          pattern = 'Наименование товара, работы, услуги')) + 11][1]
+        
+        ### 16 столбец -- объект закупки: цена за единицу
+        df.part$ObjectUnitPrice[i] <- raw.notice[which(grepl(raw.notice, 
+                                                             pattern = 'Наименование товара, работы, услуги')) + 12][1]
+        
+        ### 17 столбец -- начальная (максимальная) цена контракта
+        df.part$ObjectNMZ[i] <- raw.notice[which(grepl(raw.notice, 
+                                                       pattern = 'цена контракта')) + 1]
+        
+        ### 18 столбец -- есть ли ограничения допуска
+        txt.restr <- raw.notice[which(grepl(raw.notice, 
+                                            pattern = 'Условия, запреты и ограничения допуска'))]
+        if (length(txt.restr) != 0) {
+            df.part$IsRestrictions[i] <- T
+        }
+        
+        # загружаем текст html-страницы с координатами заказчика
+        destFileURL3 <- paste0('http://zakupki.gov.ru', df.part$customerHyperlink[i])
+        destHtml <- getURL(destFileURL3, .encoding = 'UTF-8',
+                           ssl.verifypeer = FALSE, 
+                           .opts = list(useragent = userAgent,
+                                        followlocation = TRUE))
+        ## разбираем как html
+        descDoc <- htmlTreeParse(destHtml, useInternalNodes = T)
+        ## корневой элемент
+        descRootNode <- xmlRoot(descDoc)
+        
+        ### 8 столбец -- ОКТМО заказчика
+        txt.oktmo <- xpathApplyAndClean(descRootNode, 
+                                        "//*[@id = 'tab-info']//td")
+        df.part$CustomerOKTMO <- txt.oktmo[which(txt.oktmo == 'ОКТМО') + 1]
+        
+        
+        # загружаем текст html-страницы с результатами определения поставщика
+        destHtml <- getURL(destFileURL2[i], .encoding = 'UTF-8',
+                           ssl.verifypeer = FALSE, 
+                           .opts = list(useragent = userAgent,
+                                        followlocation = TRUE))
+        
+        ## разбираем как html
+        descDoc <- htmlTreeParse(destHtml, useInternalNodes = T)
+        ## корневой элемент
+        descRootNode <- xmlRoot(descDoc)
+
+        raw.partisipants <- xpathApplyAndClean(descRootNode, "//td")
+        raw.partisipants <- raw.partisipants[(which(grepl(raw.partisipants,
+                                                          pattern = 'Протокол определения поставщика')) + 6):(which(grepl(raw.partisipants,
+                                                                                                                          pattern = 'Информация о контракте')) - 1)]
+        prices <- raw.partisipants[seq(4, 
+                                       floor(length(raw.partisipants) / 4) * 4, 
+                                       by = 4)]
+        prices <- gsub(pattern = 'Российский рубль', replacement = '', prices)
+        prices <- gsub('\\s', '', prices)
+        prices <- as.numeric(gsub(',', '.', prices))
+        
+        ### 19 столбец -- минимальное ценовое предложение
+        df.part$MinPriceOffer <- min(prices)
+        
+        ### 20 столбец -- максимальное ценовое предложение
+        df.part$MinPriceOffer <- max(prices)
+        
+        ### 21 столбец -- количество участников
+        df.part$ParticipantsNumber <- length(xpathApplyAndClean(descRootNode, 
+                                                                "//div[@class = 'noticeTabBoxWrapper']//tbody/tr"))
+    }
     
-    ## разбираем как html
-    descDoc <- htmlTreeParse(destHtml, useInternalNodes = T)
-    ## корневой элемент
-    descRootNode <- xmlRoot(descDoc)
     
-    ### 9 столбец (запись) -- наименование электронной площадки
-    df.part$PlatformName <- xpathApplyAndClean(descRootNode,
-                                               "//td")
-    
+    # ЗАПИСЬ РЕЗУЛЬТАТОВ -----------------------------------------------------------
+    filePath <- gsub(paste0('./', exportCsvFileName), 
+                     pattern = ':', 
+                     replacement = '-')
+    filePath <- gsub(filePath, 
+                     pattern = '\\s', 
+                     replacement = '_')
+    if (!file.exists(filePath)) {
+        file.create(filePath)
+        write.table(df.part, filePath, row.names = F, append = T, dec = ',', 
+                    sep = ';')
+    } else {
+        write.table(df.part, filePath, row.names = F, append = T, dec = ',', 
+                    sep = ';', col.names = F)
+    }    
 }
 
-
-# ЗАПИСЬ РЕЗУЛЬТАТОВ -----------------------------------------------------------
-filePath <- gsub(paste0('./', exportCsvFileName), 
-                 pattern = ':', 
-                 replacement = '-')
-filePath <- gsub(filePath, 
-                 pattern = '\\s', 
-                 replacement = '_')
-if (!file.exists(filePath)) {
-    file.create(filePath)
-    write.table(df.part, filePath, row.names = F, append = T, dec = ',', 
-                sep = ';')
-} else {
-    write.table(df.part, filePath, row.names = F, append = T, dec = ',', 
-                sep = ';', col.names = F)
-}
 
