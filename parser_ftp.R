e2a57dd9fa88d7f7d31bc8be36cd25c22a147ccd
aksyuk <s.a.aksuk@gmail.com>
2018-03-26 18:32:03 +0300

    xml parser updated
    dir structure parsing added

--- parser_ftp.R
+++ parser_ftp.R
@@ -6,6 +6,7 @@
 
 library('RCurl')
 library('XML')
+library('dplyr')
 
 # ## имя браузера для маскировки запроса по getURL()
 # userAgent <- 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'
@@ -15,6 +16,9 @@ library('XML')
 
 url <- 'ftp://ftp.zakupki.gov.ru/'
 userpwd <- 'free:free'
+
+# год, за который грузим документацию
+YEAR <- 2018
 
 # список директорий с регионами
 region.folders.names <- unlist(strsplit(getURL(paste0(url, 'fcs_regions/'), 
@@ -29,7 +33,7 @@ length(region.folders.names)
 # список файлов в папке
 #  регион: Московская область
 region.folder.url <- paste0('ftp://ftp.zakupki.gov.ru/fcs_regions/',
-                            region.folders.names, '/contracts/')
+                            region.folders.names, '/')
 
 df.summary.contracts <- data.frame(region = region.folders.names,
                                    number_of_contracts_2017 = 0)
@@ -38,136 +42,162 @@ if (!file.exists('./data/raw')) {
     dir.create('./data/raw')
 }
 
-# выгрузка xml в архивах в папку './data/raw/' в рабочей директории
-for (iRegion in 1:length(region.folder.url)) {
-    zips <- unlist(strsplit(getURL(region.folder.url[iRegion],  
-                                   ftp.use.epsv = FALSE, 
-                                   dirlistonly = TRUE, userpwd = userpwd),
-                            '\r\n'))
-    # берём заявки, открытые в 2018 году
-    zips <- grep(zips, pattern = '_2018.*_.*_.*', value = T)
+# имена папок с архивами
+sSubfolders <- c('notifications/', 'protocols/', 'contracts/')
 
-    for (iContract in 1:length(zips)) {
-        download.file(paste0('ftp://', userpwd, '@',
-                             gsub('^.*://', '', region.folder.url[iRegion]),
-                             zips[iContract]),
-                      destfile = paste0('./data/raw/', zips[iContract]))        
+# папка для загрузки в /data/raw
+path <- paste0('./data/raw/', YEAR, '_',
+                format(Sys.time(), '%d-%m-%Y'), '/')
+if (!dir.exists(path)) {
+    dir.create(path)
+}
+
+# выгрузка xml в архивах в папку './data/raw/' в рабочей директории
+for (sRegion in region.folder.url[1]) {
+    
+    for (sSubf in sSubfolders) {
+        zips <- unlist(strsplit(getURL(paste0(sRegion, sSubf),  
+                                       ftp.use.epsv = FALSE, 
+                                       dirlistonly = TRUE, userpwd = userpwd),
+                                '\r\n'))
+        # берём заявки, открытые в YEAR году
+        zips <- grep(zips, pattern = paste0('_', YEAR, '.*_.*_.*'), value = T)
+        
+        for (sZip in zips) {
+            download.file(paste0('ftp://', userpwd, '@',
+                                 gsub('^.*://', '', sRegion), sSubf, sZip),
+                          destfile = paste0(path, sZip))        
+        }
     }
 }
 
 # залезаем в дебри архивов
 #  индексируем всё это богатство
-path <- './data/raw/2018_19-02-2018/'
 xmlzips <- dir(path)
 tmppath <- paste0(path, 'tmp')
-n <- length(xmlzips)
-
-# ВНИМАНИЕ: это очень длинный цикл!
-for (iXMLzipCount in 1:length(xmlzips)) {
-    
-    # шаг 1: распаковать архив в tmp. там до кучи xml-файлов
-    unzip(paste0(path, xmlzips[iXMLzipCount]), exdir = tmppath)
-    xmls <- grep(dir(tmppath), pattern = 'xml$', value = T)
-
-    # шаг 2: сделать рыбу таблицы
-    df.xmls <- NULL
-    df.xmls.line <- 
-        data.frame(id = '', publishDate = '', notificationNumber = '',
-                   currentContractStage = '',
-                   customer.name = '', customer.inn = '',
-                   OKPD2.code = '', OKPD2.name = '',
-                   OKEI.code = '', OKEI.fullName = '', 
-                   quantity = 0, sumRUR = 0,
-                   supplier.name = '', supplier.inn = '', 
-                   supplier.OKTMO.code = '', supplier.OKTMO.name = '',
-                   stringsAsFactors = F)
-    
-    for (j in 1:length(xmls)) {
-        # шаг 3: запарсить распакованный xml
-        rootnode <- xmlTreeParse(paste0(tmppath, '/', xmls[j]), 
-                                 useInternalNodes = T)
-
-        # танцы с namespace
-        nsDefs <- xmlNamespaceDefinitions(rootnode)
-        ns <- structure(sapply(nsDefs, function(x) x$uri), names = names(nsDefs))
-        names(ns)[1] <- "xmlns"
-                
-        # отсев XML по типу записей
-        ls <- getNodeSet(rootnode, "//xmlns:singleCustomer|//xmlns:executions|//xmlns:cancelledProcedureId", 
-                         ns)
-        if (length(ls) != 0) break
-        
-        # считываем теги
-        tag <- getNodeSet(rootnode, "//xmlns:id", ns)
-        df.xmls.line[1, 'id'] <- ifelse(length(tag) == 0, NA, 
-                                        xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:publishDate", ns)
-        df.xmls.line[1, 'publishDate'] <- ifelse(length(tag) == 0, NA, 
-                                                 xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:notificationNumber", ns)
-        df.xmls.line[1, 'notificationNumber'] <- ifelse(length(tag) == 0, NA, 
-                                                        xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:currentContractStage", ns)
-        df.xmls.line[1, 'currentContractStage'] <- ifelse(length(tag) == 0, NA,
-                                                          xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:customer//xmlns:fullName", ns)
-        df.xmls.line[1, 'customer.name'] <- ifelse(length(tag) == 0, NA,
-                                                   xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:customer//xmlns:inn", ns)
-        df.xmls.line[1, 'customer.inn'] <- ifelse(length(tag) == 0, NA,
-                                                  xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:product/xmlns:OKPD2/xmlns:code", ns)
-        df.xmls.line[1, 'OKPD2.code'] <- ifelse(length(tag) == 0, NA,
-                                                xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:product/xmlns:OKPD2/xmlns:name", ns)
-        df.xmls.line[1, 'OKPD2.name'] <- ifelse(length(tag) == 0, NA,
-                                                xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:product/xmlns:OKEI/xmlns:code", ns)
-        df.xmls.line[1, 'OKEI.code'] <- ifelse(length(tag) == 0, NA,
-                                               xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:product/xmlns:OKEI/xmlns:fullName", ns)
-        df.xmls.line[1, 'OKEI.name'] <- ifelse(length(tag) == 0, NA,
-                                               xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:product/xmlns:quantity", ns)
-        df.xmls.line[1, 'quantity'] <- ifelse(length(tag) == 0, NA,
-                                              xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:product/xmlns:sumRUR", ns)
-        df.xmls.line[1, 'sumRUR'] <- ifelse(length(tag) == 0, NA,
-                                            xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:suppliers//xmlns:fullName", ns)
-        df.xmls.line[1, 'supplier.name'] <- ifelse(length(tag) == 0, NA,
-                                                   xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:suppliers//xmlns:INN", ns)
-        df.xmls.line[1, 'supplier.inn'] <- ifelse(length(tag) == 0, NA,
-                                                  xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:suppliers//xmlns:OKTMO/xmlns:code", ns)
-        df.xmls.line[1, 'supplier.OKTMO.code'] <- ifelse(length(tag) == 0, NA,
-                                                         xmlValue(tag[[1]]))
-        tag <- getNodeSet(rootnode, "//xmlns:suppliers//xmlns:OKTMO/xmlns:name", ns)
-        df.xmls.line[1, 'supplier.OKTMO.name'] <- ifelse(length(tag) == 0, NA,
-                                                         xmlValue(tag[[1]]))
-        
-        # записываем значения во фрейм
-        df.xmls <- rbind(df.xmls, df.xmls.line)
-    }
-    
-    # шаг 4: экспортировать в csv
-    message(paste0('Строк в ', xmlzips[iXMLzipCount], ': ', nrow(df.xmls), ''))
-    if (nrow(df.xmls) > 0) {
-        write.csv(df.xmls, 
-                  file = paste0('./data/', 
-                                gsub(xmlzips[iXMLzipCount], pattern = '.xml.zip', 
-                                     replacement = ''), 
-                                '.csv'), row.names = F)
-    }
-
-    # шаг 5: удалить содержимое папки tmp
-    rem.status <- file.remove(paste0(tmppath, '/', dir(tmppath)))
-    
-    # шаг 6: сообщение в консоль
-    message(paste0('Архив ', iXMLzipCount, ' из ', n, 
-                   ' (', round(iXMLzipCount / n * 100, 1), '%)'))
+if (!dir.exists(tmppath)) {
+    dir.create(tmppath)
 }
+
+# набор xpath запросов для парсинга xml: архивы contracts
+xpath.patterns <- 
+    c('//xmlns:id', '//xmlns:publishDate', '//xmlns:notificationNumber', 
+      '//xmlns:currentContractStage', '//xmlns:customer//xmlns:fullName', 
+      '//xmlns:customer//xmlns:inn', '//xmlns:product/xmlns:OKPD2/xmlns:code',
+      '//xmlns:product/xmlns:OKPD2/xmlns:name', 
+      '//xmlns:product/xmlns:OKEI/xmlns:code', 
+      '//xmlns:product/xmlns:OKEI/xmlns:fullName',
+      '//xmlns:product/xmlns:quantity', '//xmlns:product/xmlns:sumRUR',
+      '//xmlns:suppliers//xmlns:fullName', '//xmlns:suppliers//xmlns:INN',
+      '//xmlns:suppliers//xmlns:OKTMO/xmlns:code', 
+      '//xmlns:suppliers//xmlns:OKTMO/xmlns:name')
+names(xpath.patterns) <- c('id', 'publishDate', 'notificationNumber',
+                           'currentContractStage', 'customer.name',
+                           'customer.inn', 'OKPD2.code', 'OKPD2.name',
+                           'OKEI.code', 'OKEI.fullName', 'quantity', 'sumRUR',
+                           'supplier.fullName', 'supplier.INN', 
+                           'supplier.OKTMO.code', 'supplier.OKTMO.name')
+
+# распаковываем архивы в tmp
+for (sXMLzip in xmlzips) {
+    # шаг 1: распаковать архив в tmp. там до кучи xml-файлов
+    unzip(paste0(path, sXMLzip), exdir = tmppath)
+}
+
+# отбираем только xml с контрактами
+xmls <- grep(dir(tmppath), pattern = '^contract.*xml$', value = T)
+
+# шаг 2: сделать рыбу таблицы
+ls.xmls <- list(0)
+i.ls.xmls <- 1
+excluded.single <- NULL
+excluded.cancel <- 0
+
+# парсим xml-файлы
+# ВНИМАНИЕ: это очень длинный цикл!
+for (xml.file in xmls[1:10]) {
+    # шаг 3: запарсить распакованный xml
+    rootnode <- xmlTreeParse(paste0(tmppath, '/', xml.file), 
+                             useInternalNodes = T)
+
+    # танцы с namespace
+    nsDefs <- xmlNamespaceDefinitions(rootnode)
+    ns <- structure(sapply(nsDefs, function(x) x$uri), names = names(nsDefs))
+    names(ns)[1] <- "xmlns"
+                
+    # отсев XML по типу записей
+    #  выкидываем закупки у единственного поставщика
+    lst <- getNodeSet(rootnode, '//xmlns:singleCustomer', ns)
+    if (length(lst) != 0) {
+        excluded.single <- c(excluded.single, sapply(lst, xmlValue))
+        next
+    }
+    # выкидываем отменённые закупки тоже, считаем их
+    if (length(getNodeSet(rootnode, 
+                          '//xmlns:executions//xmlns:cancelledProcedureId', 
+                          ns)) != 0) {
+        excluded.cancel <- excluded.cancel + 1
+        next
+    }
+        
+    # считываем теги
+    tags <- getNodeSet(rootnode, xpath.patterns, ns)
+        
+    # имена родительских тегов для правильного формирования имён столбцов
+    parent.names <- xpathSApply(rootnode, xpath.patterns, function(x){
+        xmlName(xmlParent(x))
+        }, namespaces = ns)
+        
+    df.xmls.line <- as.data.frame(lapply(tags, xmlValue), 
+                                  stringsAsFactors = F)
+    colnames(df.xmls.line) <- 
+        paste(parent.names, sapply(tags, xmlName), sep = '.')
+    
+    # шаг 4: найти связанные xml с протоколами
+    notice.id <- substr(xml.file, gregexpr('_', xml.file)[[1]][1] + 1, 
+                        gregexpr('_', xml.file)[[1]][2] - 1)
+    
+    xml.related <- grep(dir(tmppath), pattern = paste0(notice.id, '.*xml'), 
+                        value = T)
+    xml.related <- xml.related[!xml.file %in% xml.related]
+    
+    if (length(xml.related) > 0) {
+        df.xmls.line$xmls.related <- paste(xml.related, collapse = '#')
+    } else {
+        df.xmls.line$xmls.related <- NA
+    }
+    
+    # записываем значения во фрейм
+    ls.xmls[[i.ls.xmls]] <- df.xmls.line
+    names(ls.xmls)[i.ls.xmls] <- paste0('notice.id:', notice.id)
+    i.ls.xmls <- i.ls.xmls + 1
+    
+    message(paste0(notice.id, ' ... ', 
+                   round(which(xmls == xml.file) / length(xmls) * 100, 1),
+                   '% done'))
+}
+    
+
+
+
+
+# шаг 6: экспортировать в csv
+message(paste0('Строк в ', sXMLzip, ': ', nrow(df.xmls), ''))
+if (!is.null(df.xmls)) {
+    write.csv(df.xmls, 
+              file = paste0('./data/', 
+                            gsub(sXMLzip, pattern = '.xml.zip', 
+                            replacement = ''), '.csv'), row.names = F)
+}
+
+# шаг 5: удалить содержимое папки tmp
+rem.status <- file.remove(paste0(tmppath, '/', dir(tmppath)))
+    
+# шаг 6: сообщение в консоль
+message(paste0('Архив ', which(xmlzips == sXMLzip), ' из ', length(xmlzips), 
+               ' (',  
+               round(which(xmlzips == sXMLzip) / length(xmlzips) * 100, 1),
+               '%)'))
 
 ###  Архив 211 из 1345 (15.7%)
 ### Строк в contract_Evrejskaja_Aobl_2018010100_2018020100_001.xml.zip: 
